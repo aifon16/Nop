71ca837a3941a65f2d2720d5926c9a13fad883b2
aifon16 <zharit46@gmail.com>
2025-05-07 20:43:19 -0500

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class EstudianteService {
+    private List<Estudiante> estudiantes = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void agregarEstudiante() {
+        System.out.print("ID: ");
+        int id = scanner.nextInt(); scanner.nextLine();
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Edad: ");
+        int edad = scanner.nextInt(); scanner.nextLine();
+        System.out.print("Carrera: ");
+        String carrera = scanner.nextLine();
+
+        estudiantes.add(new Estudiante(id, nombre, edad, carrera));
+        System.out.println("Estudiante agregado.");
+    }
